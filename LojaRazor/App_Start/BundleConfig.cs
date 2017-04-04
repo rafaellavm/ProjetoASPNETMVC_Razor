a14c14ce3766d18ddb0cca4b32cee657d51bfa4e
Rafaela <rafaela.vm@gmail.com>
2017-04-03 22:01:18 -0300

    6 otimização das views

--- LojaRazor/App_Start/BundleConfig.cs
+++ LojaRazor/App_Start/BundleConfig.cs
@@ -8,7 +8,18 @@ namespace LojaRazor
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            //juntando os css
+            StyleBundle estilos = new StyleBundle("~/bundle/estilos");
+            estilos.Include("~/Content/bootstrap/css/bootstrap.css");
+            estilos.Include("~/Content/Style.css");
+            bundles.Add(estilos);
+
+            //junntando os javascript
+            ScriptBundle scripts = new ScriptBundle("~/bundle/scripts");
+            scripts.Include("~/Scripts/jquery-1.7.1.js");
+            scripts.Include("~/Scripts/jjquery.validate.js");
+            scripts.Include("~/Scripts/jquery.validate.unobtrusive.js");
+            bundles.Add(scripts);
         }
     }
 }
